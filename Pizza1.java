269f05ce1a2ac684e94211239f54e83681a18330
kshikha818 <35431975+kshikha818@users.noreply.github.com>
2019-03-03 21:55:03 +0530

    Add files via upload

---
+++ Pizza1.java
@@ -0,0 +1,13 @@
+public class Pizza1 { 
+    int rows;
+    int cols;
+    int minIngredientEachPerSlice;
+    int maxCellsPerSlice;
+    HashMap<String, Cell> cells;
+    int rowLength;
+    int colLength;
+    
+    public String getCellHashKey (int x, int y) {
+        return String.format("%0" + rowLength + "d", x) +    String.format("%0" + colLength + "d", y);
+    }
+}
